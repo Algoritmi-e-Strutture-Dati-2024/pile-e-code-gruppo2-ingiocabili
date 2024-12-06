9bff0c92abd014c54aeebd485a8edf04f5281d1e
Samuele002 <samueledicomite2002@gmail.com>
2024-12-06 11:32:52 +0100

    Update pila.cpp

--- pila.cpp
+++ pila.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-// Definizione della classe Lista come template
 template <typename T>
 class Pila {
   private:
     //usiamo  un'istanza di ListaPunatori come base
     ListaPuntatori<T> lista;
   public:
-    // Costruttore
     void push(T elem){
-      //usa l'oggetto lista con la sua relativa funzione per inserire un elemento nella posizione in testa
       lista.inslista(elem, 1);
     }
 
@@ -17,7 +14,6 @@ class Pila {
       if(lista.listavuota()){
           return lista.lung==0;
       }else{
-        //qui viene letto il valore in testa e viene cancellato
         T valore=lista.leggilista(1);
         lista.canclista(1);
         return valore;
